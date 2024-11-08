f815967d45ce0fd0d90219887b5ca6afb95875f9
Roman Rusin <rgromkorusin12@gmail.com>
2024-11-08 12:36:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,25 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  if (typeof data === 'object' || data === null) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const li = document.createElement('li');
+
+      li.textContent = key;
+
+      if (typeof data[key] === 'object' && data[key] !== null) {
+        createTree(li, data[key]);
+      }
+
+      ul.appendChild(li);
+    }
+  }
 }
 
 createTree(tree, food);
